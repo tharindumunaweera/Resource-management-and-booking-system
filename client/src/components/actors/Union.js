a4f3fe1355e04bbbc32e87ac506a2f03e7cbd6f0
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-03-12 22:32:22 +0530

    frontend start union

---
+++ client/src/components/actors/Union.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Union extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-4">
+            <h3 className="text-center text-info">Experience</h3>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <h4>Microsoft</h4>
+              </li>
+              <li className="list-group-item">
+                <h4>Sun Microsystems</h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Union;
