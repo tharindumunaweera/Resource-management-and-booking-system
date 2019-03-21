7526a59038beccd60184c8a3b554c82958926b0a
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-03-21 20:09:46 +0530

    create spinner gif  to show loading

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -32,7 +33,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
+    //  Clear current Profile
+    store.dispatch(clearCurrentProfile);
     // Redirect to login
     window.location.href = "/login";
   }
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from "./types";
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  GET_ERRORS,
+  CLEAR_CURRENT_PROFILE
+} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -24,5 +29,11 @@ export const getCurrentProfile = () => dispatch => {
 export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
+  };
+};
+
+export const clearCurrentProfile = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE
   };
 };
