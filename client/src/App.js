b8461eab662d70ce9c866c8e967d8e63fee2f79e
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-03-22 05:30:06 +0530

    add admin panal

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Routes from '../src/components/Routes';
+import TopNavigation from './components/topNavigation';
+import SideNavigation from './components/sideNavigation';
+import Footer from './components/Footer';
+import './index.css';
+
+
 import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
+//import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Union from "./components/actors/Union";
 import Lecturer from "./components/actors/Lecturer";
 import Calender from "./components/Calender/Calender";
+import Availabilitytable from "./components/Tables/Availabilitytable";
+import BookingForm from "./components/BookingForm/BookingForm";
+
 
 import "./App.css";
 
@@ -26,6 +36,17 @@ class App extends Component {
 
   render() {
     return (
+      <div className="flexible-content">
+      <TopNavigation />
+      <SideNavigation />
+      <main id="content" className="p-5">
+        <Routes />
+      </main>
+      <Footer />
+    
+
+
+
       <Router>
         <div className="App">
             <Navbar />
@@ -35,13 +56,16 @@ class App extends Component {
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/union" component={Union} />
                 <Route exact path="/Lecturer" component={Lecturer} />
-                        
+                <Route exact path="/Availabilitytable" component={Availabilitytable } />
+                <Route exact path="/BookingForm" component={BookingForm} />
             </div>
-                <Calender style= {style} width="302px" onDayClick={(e, day)=> this.onDayClick(e,day)}  />
-
+            <Calender style= {style} width="302px" onDayClick={(e, day)=> this.onDayClick(e,day)}  />  
             <Footer />
-          </div>
+            
+        </div>
+        
       </Router>
+      </div>
       
     );
   }
