959eda4079a55e15fdff2f81e32f9760f40f994d
buddinisansala <nisansala940914@gmail.com>
2019-04-23 13:52:50 +0530

    create hallregisterschema model

--- models/Hallreg.js
+++ models/Hallreg.js
@@ -3,118 +3,32 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const HallRegisterSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "users"
-  },
-  handle: {
+  
+  hallname: {
     type: String,
-    required: true,
-    max: 40
-  },
-  company: {
-    type: String
-  },
-  website: {
-    type: String
+    required: true
   },
   location: {
-    type: String
-  },
-  status: {
     type: String,
     required: true
   },
-  skills: {
-    type: [String],
+  seat: {
+    type: String,
     required: true
   },
-  bio: {
-    type: String
+  projecter: {
+    type: String,
+    required: true
   },
-  githubusername: {
-    type: String
+  whiteboard: {
+    type: String,
+    required: true
   },
-  experience: [
-    {
-      title: {
-        type: String,
-        required: true
-      },
-      company: {
-        type: String,
-        required: true
-      },
-      location: {
-        type: String
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
-  education: [
-    {
-      school: {
-        type: String,
-        required: true
-      },
-      degree: {
-        type: String,
-        required: true
-      },
-      fieldofstudy: {
-        type: String,
-        required: true
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
-  social: {
-    youtube: {
-      type: String
-    },
-    twitter: {
-      type: String
-    },
-    facebook: {
-      type: String 
-    },
-    linkedin: {
-      type: String
-    },
-    instagram: {
-      type: String
-    }
+  other: {
+    type: String,
+    required: true
   },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+ 
 });
 
-module.exports = Profile = mongoose.model("profile", ProfileSchema);
+module.exports =  mongoose.model("HallRegisterSchema", HallRegisterSchema);
