5f0d38a06ee13a5debf86544da6d47a59edcd313
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-04-29 19:57:11 +0530

    prepared for deploy

--- server.js
+++ server.js
@@ -118,4 +118,14 @@ hallRegRouts.route("/update/:id").post(function(req, res) {
 
 app.use("/todos", hallRegRouts);
 
+//server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
