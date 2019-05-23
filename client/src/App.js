9e10762b1aae2d8b5dac40aff977a397864805c4
buddinisansala <30474389+buddinisansala@users.noreply.github.com>
2019-05-23 17:39:05 +0530

    for get pull request

--- client/src/App.js
+++ client/src/App.js
@@ -111,7 +111,7 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <Navbar />
+            {/* <Navbar /> */}
             <Route exact path="/" component={Landing} />
             {/* <div className="container "> */}
 
