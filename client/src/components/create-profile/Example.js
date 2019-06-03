33c03235018d3c5f11fa2825d1db810280698b93
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-06-03 22:28:16 +0530

    booking set 10,11 times

--- client/src/components/create-profile/Example.js
+++ client/src/components/create-profile/Example.js
@@ -272,11 +272,11 @@ class Example extends Component {
           if (bookings === null || loading) {
             nineInputs = <Spinner />;
           } else {
-            let em = 0; let time="09.00 a.m-10.00 a.m";
+            let em = 0; let time = "09.00 a.m-10.00 a.m";
             if (bookings.length > 0) {
               nineInputs = bookings.map(booking => (
                 <div>
-                  {(this.state.hallname === booking.hallname) && (this.state.bookdate === booking.bookdate) && (time === booking.booktime)? (
+                  {(this.state.hallname === booking.hallname) && (this.state.bookdate === booking.bookdate) && (time === booking.booktime) ? (
                     em = 1,
                     <table class="table table-light table-striped">
                       <tbody>
@@ -347,32 +347,77 @@ class Example extends Component {
 
       for (i = 0; i <= nine.Mtentoeleven.length; i++) {
         if (nine.Mtentoeleven[i] == this.state.hallname) {
-          ten = true;
-          tenInputs = (
-            <div>
-              <table class="table table-light table-striped">
-                <tbody>
-                  <tr>
-                    <th scope="row">10.00 a.m-11.00 a.m</th>
-                    <td><div>
-                      <Link to={{
-                        pathname: './createbooking',
-                        state: {
-                          hallnamebook: this.state.hallname,
-                          bkdate: this.state.bookdate,
-                          bktime: "10.00 a.m-11.00 a.m"
+          if (bookings === null || loading) {
+            tenInputs = <Spinner />;
+          } else {
+            let em = 0; let time = "10.00 a.m-11.00 a.m";
+            if (bookings.length > 0) {
+              tenInputs = bookings.map(booking => (
+                <div>
+                  {(this.state.hallname === booking.hallname) && (this.state.bookdate === booking.bookdate) && (time === booking.booktime) ? (
+                    em = 1,
+                    <table class="table table-light table-striped">
+                      <tbody>
+                        <tr>
+                          <th scope="row">10.00 a.m-11.00 a.m</th>
+                          <td><div>
+                            <Link to={{
+                              pathname: './createbooking',
+                              state: {
+                                hallnamebook: this.state.hallname,
+                                bkdate: this.state.bookdate,
+                                bktime: "10.00 a.m-11.00 a.m",
 
-                        }
-                      }}>
-                        Available
+
+                              }
+                            }}>
+                              Booking
+                  </Link>
+
+                          </div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  ) : null}
+
+
+                </div>
+
+              ));
+            }
+
+            if (em === 0) {
+              ten = true;
+              tenInputs = (
+                <div>
+                  <table class="table table-light table-striped">
+                    <tbody>
+                      <tr>
+                        <th scope="row">10.00 a.m-11.00 a.m</th>
+                        <td><div>
+                          <Link to={{
+                            pathname: './createbooking',
+                            state: {
+                              hallnamebook: this.state.hallname,
+                              bkdate: this.state.bookdate,
+                              bktime: "10.00 a.m-11.00 a.m",
+
+
+                            }
+                          }}>
+                            Available
                       </Link>
 
-                    </div></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          );
+                        </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              );
+            }
+          }
         }
       }
 
